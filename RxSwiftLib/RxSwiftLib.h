9836186c11eeee1f44e1cb1b2b5297588c8b8027
Indrajeet Senger <indrajeet.senger@pc0364.local>
2021-05-04 11:16:53 +0530

    Initial project setup

---
+++ RxSwiftLib/RxSwiftLib.h
@@ -0,0 +1,18 @@
+//
+//  RxSwiftLib.h
+//  RxSwiftLib
+//
+//  Created by Indrajeet Senger on 04/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxSwiftLib.
+FOUNDATION_EXPORT double RxSwiftLibVersionNumber;
+
+//! Project version string for RxSwiftLib.
+FOUNDATION_EXPORT const unsigned char RxSwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwiftLib/PublicHeader.h>
+
+
